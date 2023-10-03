1bbbb63256a8673e3852d334f1f2c432d4bc4488
crawlic-stud <nikitosik0726@gmail.com>
2023-10-03 18:37:46 +0300

    backdoor for old map

--- src/api/user_map.py
+++ src/api/user_map.py
@@ -9,11 +9,12 @@ from config import (
     MAPBOX_STYLE,
 )
 import utils
+from models import LocationInDb
+from services import map_display
 
 
-@app.get("/data/{user_id}")
 @app.get("/map/{user_id}")
-async def get_map_for_user(user_id: int, request: Request) -> HTMLResponse:
+async def get_default_for_user(user_id: int, request: Request) -> HTMLResponse:
     raw_locs = await utils.db_get_all(locations_db, {"user_id": user_id})
     return templates.TemplateResponse(
         "map.html",
@@ -23,3 +24,11 @@ async def get_map_for_user(user_id: int, request: Request) -> HTMLResponse:
             "access_token": MAPBOX_TOKEN,
             "map_style": MAPBOX_STYLE
         })
+
+
+@app.get("/default/map/{user_id}")
+async def get_default_for_user(user_id: int) -> HTMLResponse:
+    raw_locs = await utils.db_get_all(locations_db, {"user_id": user_id})
+    locs = utils.convert_dicts_to_type(raw_locs, LocationInDb)
+    html = map_display.get_map_html(locs)
+    return HTMLResponse(content=html)
