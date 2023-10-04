6e073d4832d5a56de4d35ea36fc000a4c79197f8
crawlic-stud <nikitosik0726@gmail.com>
2023-10-04 21:37:35 +0300

    feta: added delete button

--- src/api/user_map.py
+++ src/api/user_map.py
@@ -1,5 +1,6 @@
 from fastapi.responses import HTMLResponse
-from fastapi import Request
+from fastapi import Request, Response
+from bson import ObjectId
 
 from config import (
     app,
@@ -34,3 +35,13 @@ async def get_default_for_user(user_id: int) -> HTMLResponse:
     locs = utils.convert_dicts_to_type(raw_locs, LocationInDb)
     html = map_display.get_map_html(locs)
     return HTMLResponse(content=html)
+
+
+@app.delete("/location/{loc_id}")
+async def delete_location(
+    loc_id: str,
+) -> Response:
+    res = await locations_db.delete_one({"_id": ObjectId(loc_id)})
+    if res.deleted_count:
+        return Response(status_code=200)
+    return Response(status_code=400)
