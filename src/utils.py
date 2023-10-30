5922a60d0953ce239ce6df6b53fe43d97726bdd4
crawlic-stud <nikitosik0726@gmail.com>
2023-10-31 00:10:40 +0300

    refactor

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ from pymongo.collection import Collection
 from aiogram import Bot
 from aiogram.types import MenuButtonWebApp, WebAppInfo
 
-from config import locations_db, MAPS_BASE_URL, MAPS_BUTTON_NAME
+from config import locations_db, logger, MAPS_BASE_URL, MAPS_BUTTON_NAME
 from models import IconInfo
 
 
@@ -22,21 +22,23 @@ async def db_get_all(db: Collection, _filter: dict[str, Any]):
     return result
 
 
+async def set_menu_button(bot: Bot, user_id: int | None = None):
+    await bot.set_chat_menu_button(
+        chat_id=user_id,
+        menu_button=MenuButtonWebApp(
+            text=MAPS_BUTTON_NAME, web_app=WebAppInfo(url=f"{MAPS_BASE_URL}/{user_id}")
+        ),
+    )
+
+
 async def set_users_menu_button(bot: Bot):
     all_users = await locations_db.distinct("user_id")
-    await bot.set_chat_menu_button(
-        menu_button=MenuButtonWebApp(
-            text=MAPS_BUTTON_NAME,
-            web_app=WebAppInfo(url=f"{MAPS_BASE_URL}/0")
-        ))
+    await set_menu_button(bot)
     for user_id in all_users:
-        await bot.set_chat_menu_button(
-            chat_id=user_id,
-            menu_button=MenuButtonWebApp(
-                text=MAPS_BUTTON_NAME,
-                web_app=WebAppInfo(url=f"{MAPS_BASE_URL}/{user_id}")
-            )
-        )
+        try:
+            await set_menu_button(bot, user_id)
+        except Exception as e:
+            logger.error(f"Error with user {user_id}: {e}")
 
 
 def get_all_icons() -> list[IconInfo]:
